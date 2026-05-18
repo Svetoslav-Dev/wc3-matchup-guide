dd77f78d560f1f7c5443d052a6740e69aae9de9f
FilthySmugCookie <svetoslav.dichkov@gmail.com>
2026-05-18 10:35:22 +0300

    Update eslint flat config for next 15

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,9 +1,18 @@
-import coreWebVitals from "eslint-config-next/core-web-vitals";
-import typescriptConfig from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
+import coreWebVitals from "eslint-config-next/core-web-vitals.js";
+import typescriptConfig from "eslint-config-next/typescript.js";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
 
 const config = [
-  ...coreWebVitals,
-  ...typescriptConfig,
+  {
+    ignores: [".next/**", "node_modules/**", "dist/**", "coverage/**"],
+  },
+  ...compat.config({
+    extends: [...coreWebVitals.extends, ...typescriptConfig.extends],
+  }),
 ];
 
 export default config;
